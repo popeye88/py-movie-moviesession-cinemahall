33881cde66a9efa9decb8b1bf7fb5afd48d59d5c
popeye88 <aleksei.taran@gmail.com>
2024-09-30 12:21:49 +0300

    fix: fixed types and logic

--- services/movie.py
+++ services/movie.py
@@ -6,7 +6,7 @@ from db.models import Movie
 def get_movies(
         genres_ids: list[int] = None,
         actors_ids: list[int] = None
-) -> QuerySet:
+) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
 
     if genres_ids:
--- services/movie_session.py
+++ services/movie_session.py
@@ -15,7 +15,7 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet:
+def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
     manager = MovieSession.objects
 
     if session_date:
@@ -34,7 +34,7 @@ def update_movie_session(
         movie_id: int = None,
         cinema_hall_id: int = None,
 ) -> None:
-    movie_session = MovieSession.objects.get(id=session_id)
+    movie_session = get_movie_session_by_id(session_id)
 
     if show_time:
         movie_session.show_time = show_time

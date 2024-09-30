e35f53c62ca1283e90870b86e93a203bfd653a6d
popeye88 <aleksei.taran@gmail.com>
2024-09-30 12:55:27 +0300

    fix: fixed logic

--- services/movie_session.py
+++ services/movie_session.py
@@ -16,12 +16,12 @@ def create_movie_session(
 
 
 def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
-    manager = MovieSession.objects
+    queryset = MovieSession.objects.all()
 
     if session_date:
-        return manager.filter(show_time__date=session_date)
+        return queryset.filter(show_time__date=session_date)
 
-    return manager.all()
+    return queryset.all()
 
 
 def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
